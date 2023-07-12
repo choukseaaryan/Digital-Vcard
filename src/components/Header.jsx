ba57296852c10adf9b3a6d42a79ea263a0d878b0
choukseaaryan <aaryan2chouksey@gmail.com>
2023-07-12 17:06:55 +0530

    Added card body

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import WebsiteButton from "./WebsiteButton";
 function Header() {
   return (
     <div className="header-div">
-      <div className="vcard-head">
+      <div className="vcard-header">
         <div className="img-wrap">
           <img
             src="https://media.licdn.com/dms/image/C5103AQG8A20f3LN4ag/profile-displayphoto-shrink_400_400/0/1541751259170?e=1694649600&v=beta&t=JULq87kOc4ZEvuMxXWf0Fm7O1NzkFgFs-SmI94P9eHo"
