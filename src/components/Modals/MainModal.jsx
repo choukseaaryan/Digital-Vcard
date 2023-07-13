6a471eb52a1b51f01b10f137e4592364b848df9f
choukseaaryan <aaryan2chouksey@gmail.com>
2023-07-13 17:36:43 +0530

    Added hovering button on the vcard

--- src/components/Modals/MainModal.jsx
+++ src/components/Modals/MainModal.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./ModalComponent.css";
-import Button from "@mui/material/Button";
+import Button from 'react-bootstrap/Button';
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
@@ -9,50 +9,11 @@ import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 import EmailModal from './EmailModal';
 
-export default function ModalComponent() {
-  // const [isOpen, setIsOpen] = useState(false);
-
-  // const openModal = () => {
-  //   setIsOpen(true);
-  // };
-
-  // const closeModal = () => {
-  //   setIsOpen(false);
-  // };
-
-  // const openModal1 = () => {
-  //   // Logic to open modal 1
-  // };
-
-  // const openModal2 = () => {
-  //   // Logic to open modal 2
-  // };
-
-  // const openModal3 = () => {
-  //   // Logic to open modal 3
-  // };
-
-  return (
-    <div>
-      {AlertDialogSlide()}
-
-      {/* <button onClick={openModal} className="dbtn">
-          Download VCard
-        </button>
-  
-        {isOpen && (AlertDialogSlide())} */}
-
-      {/* Render additional modals based on your requirements */}
-      {/* Add logic to display different modal components */}
-    </div>
-  );
-}
-
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-function AlertDialogSlide() {
+export default function ModalComponent() {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -66,6 +27,9 @@ function AlertDialogSlide() {
   return (
     <div>
       <Button className="dbtn" onClick={handleClickOpen}>
+        Download VCard
+      </Button>
+      <Button className="fbtn" onClick={handleClickOpen}>
         Download VCard
       </Button>
       <Dialog
