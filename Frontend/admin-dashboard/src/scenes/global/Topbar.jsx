2c3fd741cc83c1f338a1732f4b8fc1096b2b01dc
choukseaaryan <aaryan2chouksey@gmail.com>
2023-07-24 18:25:12 +0530

    changed structure

---
+++ Frontend/admin-dashboard/src/scenes/global/Topbar.jsx
@@ -0,0 +1,32 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext } from "../../theme";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import Header from "../../components/Header";
+
+const Topbar = () => {
+  const theme = useTheme();
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/* ICONS */}
+      <Box display="flex">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlinedIcon />
+          ) : (
+            <LightModeOutlinedIcon />
+          )}
+        </IconButton>
+      </Box>
+    </Box>
+  );
+};
+
+export default Topbar;
---
+++ Frontend/vcard-website/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header/Header';
+import Body from './components/Body/Body';
+import { Box } from '@mui/material';
+
+function App() {
+  return (
+    <Box>
+      <Header />
+      <Body />
+    </Box>
+  );
+}
+
+export default App;
