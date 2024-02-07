ec0dd19ecb835ab2c2e1cda7c6220f76457c15ee
choukseaaryan <aaryan2chouksey@gmail.com>
2024-02-08 03:40:05 +0530

    test commit

--- Backend/middlewares/multerMiddleware.js
+++ Backend/middlewares/multerMiddleware.js
@@ -1,23 +1,55 @@
 const multer = require("multer");
 const path = require("path");
-const fs = require('fs');
+const fs = require("@cyclic.sh/s3fs");
+const multerS3 = require("multer-s3");
+const AWS = require("aws-sdk");
+const handleException = require("../utils/exception");
+let upload;
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // Define the destination directory for uploaded files
-    const dir = path.join(__dirname, "../files/profiles", `${req.decoded.id}`);
-    
-    if (!fs.existsSync(dir)){
-      fs.mkdirSync(dir, { recursive: true });
-    }
-    cb(null, path.join(__dirname, "../files/profiles", `${req.decoded.id}`));
-  },
-  filename: function (req, file, cb) {
-    // Define the filename for uploaded files
-    cb(null, `${req.body.employee_id}.png`);
-  },
-});
+try {
+	// console.log("\n\n\n\nAWS_ACCESS_KEY_ID: ", process.env.AWS_ACCESS_KEY_ID);
+	// const s3 = new AWS.S3();
 
-const upload = multer({ storage: storage });
+	const storage = multer.diskStorage({
+		destination: function (req, file, cb) {
+			// Define the destination directory for uploaded files
+			const dir = path.join(
+				__dirname,
+				"../files/profiles",
+				`${req.decoded.id}`
+			);
+
+			if (!fs.existsSync(dir)) {
+				fs.mkdirSync(dir, { recursive: true });
+			}
+			cb(
+				null,
+				path.join(__dirname, "../files/profiles", `${req.decoded.id}`)
+			);
+		},
+		filename: function (req, file, cb) {
+			// Define the filename for uploaded files
+			cb(null, `${req.body.employee_id}.png`);
+		},
+	});
+
+	upload = multer({ storage: storage });
+
+	// upload = multer({
+	// 	storage: multerS3({
+	// 		s3: s3,
+	// 		bucket: process.env.AWS_BUCKET_NAME,
+	// 		key: function (req, file, cb) {
+	// 			cb(
+	// 				null,
+	// 				`profiles/${req.decoded.id}/${req.body.employee_id}.png`
+	// 			);
+	// 		},
+	// 	}),
+	// });
+} catch (e) {
+	console.log("Error in multerMiddleware: ", e);
+	handleException(res, e);
+}
 
 module.exports = upload;
--- Backend/utils/generateQRCode.js
+++ Backend/utils/generateQRCode.js
@@ -1,14 +1,31 @@
 const QRCode = require("qrcode");
-const fs = require("fs");
+const fs = require("@cyclic.sh/s3fs/promises");
 const path = require("path");
+// const AWS = require("aws-sdk");
+
+// const s3 = new AWS.S3({
+// 	accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+// 	secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+// 	region: process.env.AWS_REGION,
+// });
 
 const generateQRCode = async (values) => {
-  const employeeId = values.employeeId;
-  const adminId = values.adminId;
-  const qrCodePath = path.join(__dirname, "../files/QRCodes", `${adminId}`, `${employeeId}.png`);
-  const vCardPath = path.join(__dirname, "../files/VCards", `${adminId}`, `${employeeId}.vcf`);
+	const employeeId = values.employeeId;
+	const adminId = values.adminId;
+	const qrCodePath = path.join(
+		__dirname,
+		"../files/QRCodes",
+		`${adminId}`,
+		`${employeeId}.png`
+	);
+	const vCardPath = path.join(
+		__dirname,
+		"../files/VCards",
+		`${adminId}`,
+		`${employeeId}.vcf`
+	);
 
-  const vcfData = `BEGIN:VCARD
+	const vcfData = `BEGIN:VCARD
 VERSION:3.0
 N:${values.lastName};${values.firstName};;;
 FN:${values.firstName} ${values.lastName}
@@ -20,23 +37,41 @@ EMAIL;TYPE=WORK:${values.email}
 URL:${values.website}
 END:VCARD`;
 
-  try {
-    console.log("Generating QR code...");
-    const qrcode_data = await QRCode.toDataURL(vcfData);
-    console.log("QR code data generated successfully!");
+	try {
+		console.log("Generating QR code...");
+		const qrcode_data = await QRCode.toDataURL(vcfData);
+		console.log("QR code data generated successfully!");
 
-    // Save QR code image to QRCodes folder
-    await fs.promises.mkdir(path.dirname(qrCodePath), { recursive: true });
-    await fs.promises.writeFile(qrCodePath, qrcode_data.split(",")[1], "base64");
+		// Save QR code image to QRCodes folder
+		await fs.promises.mkdir(path.dirname(qrCodePath), { recursive: true });
+		await fs.promises.writeFile(qrCodePath, qrcode_data.split(",")[1], "base64");
 
-    // Save VCard data to VCards folder
-    await fs.promises.mkdir(path.dirname(vCardPath), { recursive: true });
-    await fs.promises.writeFile(vCardPath, vcfData);
+		// Save VCard data to VCards folder
+		await fs.promises.mkdir(path.dirname(vCardPath), { recursive: true });
+		await fs.promises.writeFile(vCardPath, vcfData);
 
-    console.log(`QR code and VCard saved for employee ${employeeId}`);
-  } catch (err) {
-    console.error("Error generating QR code and VCard:", err);
-  }
+		// // Upload QR code image to S3 bucket
+		// const qrCodeParams = {
+		// 	Bucket: process.env.AWS_BUCKET_NAME,
+		// 	Key: `files/QRCodes/${adminId}/${employeeId}.png`,
+		// 	Body: Buffer.from(qrcode_data.split(",")[1], "base64"),
+		// 	ContentType: "image/png",
+		// };
+		// await s3.upload(qrCodeParams).promise();
+
+		// // Upload VCard data to S3 bucket
+		// const vCardParams = {
+		// 	Bucket: "your-bucket-name",
+		// 	Key: `files/VCards/${adminId}/${employeeId}.vcf`,
+		// 	Body: vcfData,
+		// 	ContentType: "text/vcard",
+		// };
+		// await s3.upload(vCardParams).promise();
+
+		console.log(`QR code and VCard saved for employee ${employeeId}`);
+	} catch (err) {
+		console.error("Error generating QR code and VCard:", err);
+	}
 };
 
 module.exports = generateQRCode;
